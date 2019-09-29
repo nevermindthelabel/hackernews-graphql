e7716345b42e48fbf1b31d59a0a4ddc95f8f6f1e
Matt Kilcup <nevermindthelabel@gmail.com>
2019-09-28 21:13:37 -0700

    added mutation

--- server.js
+++ server.js
@@ -3,12 +3,35 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]
+  }
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+  type Mutation {
+    post(url: String!, description: String!): Link!
   }
 `;
 
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Full Stack tutorial for GraphQL'
+  }
+];
+
 const resolvers = {
   Query: {
-    info: () => `This is the GraphQL API of a Hackernews clone`
+    info: () => `This is the GraphQL API of a Hackernews clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 };
 
