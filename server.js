ee55c8aa52f1aa7284662678b7990a26e25775ab
Matt Kilcup <nevermindthelabel@gmail.com>
2019-10-04 21:43:12 -0700

    imported and added Subscription resolver

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutations');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link
 };
