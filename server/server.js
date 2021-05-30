935fb7b72ee2ef729d79122ec910d3589a27e856
Matt Kilcup <nevermindthelabel@gmail.com>
2021-05-29 23:48:39 -0700

    updated with prisma

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 const path = require('path');
+
+const prisma = new PrismaClient();
 
 let links = [
   {
@@ -40,7 +43,10 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
-  resolvers
+  resolvers,
+  context: {
+    prisma
+  }
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
