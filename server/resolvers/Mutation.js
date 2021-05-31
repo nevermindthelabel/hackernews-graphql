461dd853c5126f2a02dd68879b5e87a9087ed802
Matt Kilcup <nevermindthelabel@gmail.com>
2021-05-31 13:35:17 -0700

    added post resolver

--- server/resolvers/Mutation.js
+++ server/resolvers/Mutation.js
@@ -34,6 +34,18 @@ async function login(parent, args, context, info) {
   };
 }
 
+async function post(parent, arge, context, info) {
+  const { userId } = context;
+
+  return await context.prisma.link.create({
+    data: {
+      url: args.url,
+      description: args.description,
+      postedBy: { connect: { id: userId } }
+    }
+  });
+}
+
 module.exports = {
   signup,
   login,
